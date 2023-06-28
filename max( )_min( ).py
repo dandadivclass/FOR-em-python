fb72115945458c0e0d1b373f851b0583b5f5209e
Dandara <122398596+dandadivclass@users.noreply.github.com>
2023-06-28 00:14:14 -0300

    Create max( )_min( ).py

---
+++ max( )_min( ).py
@@ -0,0 +1,8 @@
+#Faça um programa que leia 5 números e informe o maior número.
+
+lista = []
+
+for i in range(5):
+    numeros = lista.append(int(input("Digite os numeros:")))
+print(max(lista))
+print(min(lista))
